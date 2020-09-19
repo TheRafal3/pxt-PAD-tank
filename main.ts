2f718ba03e66614a65c44aff5e34c3b8a8c32ad7
Adam Grabara <44620474+agrabara@users.noreply.github.com>
2020-09-19 17:29:32 +0200

    zmiany - sterowanie, dodana klawiatura i zmiana komunikatu

--- main.ts
+++ main.ts
@@ -39,52 +39,47 @@ function doMalujBiegi(kol: number, ile: number) {
 }
 
 basic.forever(function () {
-    m504 = pins.analogReadPin(AnalogPin.P2)
+    m504 = pins.analogReadPin(AnalogPin.P2) //PRAWY
+    m505 = pins.analogReadPin(AnalogPin.P1) //LEWY
 
-    serial.writeNumber(m504)
-    m505 = pins.analogReadPin(AnalogPin.P1)
-
-
-
-
-    //lpredkosc = Math.map(kalib, 0, 1023, 0, 255)
-    if (m505 > 600) {
-        pk = 0
+    if (m505 > 600) { //kalibracja: 400-600 to ZERO, 600-1023 do przodu, 1-400 do tyłu
         lk = 1
-        kalib = Math.map(m505, 600, 1023, 20, 255)
-        ppredkosc = 255
-        lpredkosc = 255
+        kalib = Math.map(m505, 600, 1023, 20, 255) //minimalna prędkość 20
+        serial.writeLine("PRZOD LEWA")
     } else if (m505 < 400) {
-        pk = 1
         lk = 0
-        kalib = Math.map(m505, 1, 400, 20, 255)
+        kalib = Math.map(m505, 1, 400, 20, 255) //minimalna prędkość 20
         kalib = 275 - kalib
-        ppredkosc = 255
-        lpredkosc = 255
+        serial.writeLine("TYŁ LEWA")
     } else {
-        if (m504 > 584) {
-            lk = 1
-            kalib = Math.map(m504, 584, 1023, 20, 255)
-        } else if (m504 < 380) {
-            lk = 0
-            kalib = Math.map(m504, 1, 380, 20, 255)
-            kalib = 275 - kalib
-        } else {
-            lk = 0
-            kalib = 0
-        }
-        pk = lk
-        lpredkosc = Math.floor(kalib / (6 - lbieg))
+        lk = 0
+        kalib = 0
+        //serial.writeLine("STOP LEWA")
     }
-    //ppredkosc = Math.floor(kalib / (6 - pbieg))
-    //ppredkosc = Math.map(kalib, 0, 1023, 0, 255)
-    //led.plotBarGraph(
-    //    lpredkosc,
-    //    255
-    //)
-
-    ppredkosc = lpredkosc
-
+    lpredkosc = Math.floor(kalib / (6 - lbieg))
+    
+    if (m504 > 584) { //kalibracja: 380-584 to ZERO, 584-1023 do przodu, 1-380 do tyłu 
+        pk = 1
+        kalib = Math.map(m504, 584, 1023, 20, 255) //minimalna prędkość 20
+        serial.writeLine("PRZOD PRAWA")
+    } else if (m504 < 380) {
+        pk = 0
+        kalib = Math.map(m504, 1, 380, 20, 255) //minimalna prędkość 20
+        kalib = 275 - kalib
+        serial.writeLine("TYŁ PRAWA")
+    } else {
+        pk = 0
+        kalib = 0
+        //serial.writeLine("STOP PRAWA")
+    }
+    ppredkosc = Math.floor(kalib / (6 - pbieg))
+    
+    //budujemy komunikat do wysłania:
+    // LK - kierunek lewej gąsienicy (jeden znak)
+    // PL - kierunek prawej gąsienicy (jeden znak)
+    // lpredkosc - prędkość lewej gąsienicy (3 znaki)
+    // ppredkosc - prędkość prawej gąsienicy (3 znaki)
+    // 
     wyslij = lk.toString()
     wyslij = wyslij + pk.toString()
 
@@ -104,15 +99,73 @@ basic.forever(function () {
         wyslij = wyslij + ppredkosc.toString()
     }
 
-    //led.plotBarGraph(
-    //    lpredkosc,
-    //    255
-    //)
-    //serial.writeLine("l=" + parseInt(wyslij.substr(2, 3)) + " " + m504 + " w=" + parseInt(wyslij.charAt(0)))
-    serial.writeLine(wyslij)
-    radio.sendString(wyslij)
+
+  adc_key_in = pins.analogReadPin(AnalogPin.P3);    // read the value from the sensor
+  //serial.writeNumber(adc_key_in);
+  //serial.writeLine("-")
+  key = do_get_key(adc_key_in);  // convert into key press
+
+  if (key != oldkey)   // if keypress is detected
+   {
+    basic.pause(50);  // wait for debounce time
+    adc_key_in = pins.analogReadPin(AnalogPin.P3)    // read the value from the sensor
+    key = do_get_key(adc_key_in);    // convert into key press
+    if (key != oldkey)
+    {
+      oldkey = key;
+      if (key >0){
+        switch(key)
+        {
+           case 1:serial.writeLine("S1 OK");
+                wyslij = wyslij + "S1";
+                break;
+           case 2:serial.writeLine("S2 OK");
+                wyslij = wyslij + "S2";
+                break;
+           case 3:serial.writeLine("S3 OK");
+                wyslij = wyslij + "S3";
+                break;
+           case 4:serial.writeLine("S4 OK");
+                wyslij = wyslij + "S4";
+                break;
+           case 5:serial.writeLine("S5 OK");
+                wyslij = wyslij + "S5";
+                break;
+           default: wyslij = wyslij + "  "; 
+        }
+      }
+    }
+   }
+
+    radio.sendString(wyslij) //wysyłamy do serwera czołgu
 
     led.plot(2, 4)
     basic.pause(10)
     led.unplot(2, 4)
+
+
 })
+
+
+let adc_key_val =[835,860,890,920,1000];
+let NUM_KEYS = 5;
+let adc_key_in;
+let key = -1;
+let oldkey = -1;
+
+ 
+
+
+// Convert ADC value to key number
+function do_get_key(input:number) {
+    let k;
+    for (k = 0; k < NUM_KEYS; k++)
+    {
+      if (input < adc_key_val[k])
+     {
+            return k+1;
+        }
+   }
+       if (k >= NUM_KEYS)k = -1;  // No valid key pressed
+       return k;
+}
